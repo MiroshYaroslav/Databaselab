4f425eaa086b92f62b485104004b15d5904f4369
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-11-20 18:21:23 +0200

    Fixed

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,3 @@
-# app/routes/__init__.py
 from app.user.controller.user_controller import user_bp
 from app.user.controller.station_controller import station_bp
 from app.user.controller.generic_controller import generic_bp
@@ -12,18 +11,15 @@ from app.user.controller.address_controller import address_bp
 
 
 def register_routes(app):
-    # Основні ресурси
     app.register_blueprint(user_bp)
     app.register_blueprint(station_bp)
     app.register_blueprint(address_bp)
     app.register_blueprint(panel_type_bp)
     app.register_blueprint(panel_bp)
 
-    # Інші таблиці
     app.register_blueprint(battery_bp)
     app.register_blueprint(panel_generation_bp)
     app.register_blueprint(battery_charge_bp)
     app.register_blueprint(energy_sale_bp)
 
-    # Для generic CRUD (за потреби)
     app.register_blueprint(generic_bp)
